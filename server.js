93d1319ad37b92981d0af526eeae0f40c5d230bb
Ranjith K <thisisranjithk@gmail.com>
2024-12-26 18:13:56 +0530

    bootcamps api routes setup

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// const logger = require("./middleware/logger"); // custom middleware
 
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
+// Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// app.use(logger);
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
