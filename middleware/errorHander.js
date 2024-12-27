f6e6592345a7f02783abda1a986cb84e7bc2dae1
Ranjith K <thisisranjithk@gmail.com>
2024-12-27 15:54:14 +0530

    Mongoose duplicate key error handling

--- middleware/errorHander.js
+++ middleware/errorHander.js
@@ -1,14 +1,22 @@
 const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (err, req, res, next) => {
-  // console.log(err.stack.red);
+
   let error = { ...err };
   error.message = err.message;
 
+  // Mongoose bad ObjectId
   if (err.name === "CastError") {
     const message = `Bootcamp not found for ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered, Bootcamp already exits`;
+    error = new ErrorResponse(message, 409);
+  }
+
   res
     .status(error.statusCode || 500)
     .json({ success: false, error: error.message || "Server Error" });
