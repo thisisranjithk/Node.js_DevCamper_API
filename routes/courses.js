15872e46b23e678acbfa97ea1b8e2562c5a84fde
Ranjith K <thisisranjithk@gmail.com>
2024-12-30 18:38:12 +0530

    courses router

---
+++ routes/courses.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const { getCourses, getCourse } = require("../controllers/courses");
+
+router.route("/").get(getCourses);
+router.route("/:id").get(getCourse);
+
+module.exports = router;
