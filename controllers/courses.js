027ac0d99804458def719719809470ee5e11258d
Ranjith K <thisisranjithk@gmail.com>
2025-01-02 13:21:16 +0530

    advancedResults middleware integration to getCourses

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,62 +8,7 @@ const BootCamp = require("../models/BootCamp");
 // Route: GET /api/v1/bootcamps/:bootcampId/courses
 // Access: Public
 exports.getCourses = asyncHander(async (req, res, next) => {
-  let query;
-
-  const reqQuery = { ...req.query };
-  const removeFields = ["select", "sort", "page", "limit"];
-  removeFields.map((params) => delete reqQuery[params]);
-
-  if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  } else {
-    query = Course.find();
-  }
-
-  // Query for Selected Fields
-  if (req.query.select) {
-    const fields = req.query.select.replace(",", " ");
-    query = query.select(fields);
-  }
-
-  //Sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.replace(",", " ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("-updatedAt");
-  }
-
-  // Pagination
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 25;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Course.countDocuments();
-
-  query = query.skip(startIndex).limit(limit);
-
-  // Pagination result
-  const pagination = {};
-
-  if (startIndex > 0) {
-    pagination.prev = {
-      prev: page - 1,
-      limit,
-    };
-  }
-  pagination.currentPage = page;
-  if (endIndex < total) {
-    pagination.next = {
-      next: page + 1,
-      limit,
-    };
-  }
-
-  const courses = await query;
-  res
-    .status(200)
-    .json({ success: true, count: courses.length, pagination, data: courses });
+  res.status(200).json(res.advancedResults);
 });
 
 // Description: Get single Course
--- routes/courses.js
+++ routes/courses.js
@@ -7,8 +7,10 @@ const {
   updateCourse,
   deleteCourse,
 } = require("../controllers/courses");
+const Course = require("../models/Course");
+const advancedResults = require("../middleware/advancedResults");
 
-router.route("/").get(getCourses).post(addCourse);
+router.route("/").get(advancedResults(Course), getCourses).post(addCourse);
 router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
