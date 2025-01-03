50783808a72cd73e8abceb1a0f3e9eb73380bdd4
Ranjith K <thisisranjithk@gmail.com>
2025-01-03 10:58:47 +0530

    User login API

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,3 +24,38 @@ exports.register = asyncHander(async (req, res, next) => {
     token,
   });
 });
+
+// Description:  Login User
+// Route: POST /api/v1/auth/login
+// Access: Public
+exports.login = asyncHander(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return next(new ErrorResponse("Please provide the email", 404));
+  }
+
+  if (!password) {
+    return next(new ErrorResponse("Please provide the password", 404));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+
+  // MatchPassword Middleware method
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+
+  const token = user.getSignedJwtToken();
+  res.status(200).json({
+    success: true,
+    message: "User LoggedIn successfully",
+    token,
+  });
+});
--- models/User.js
+++ models/User.js
@@ -51,4 +51,9 @@ UserSchema.methods.getSignedJwtToken = function () {
   });
 };
 
+// Match user enteredPassword with hashed password in Database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("User", UserSchema);
