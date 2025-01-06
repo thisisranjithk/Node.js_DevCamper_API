cc29dca9aad3ddfe4e4a601f39b5b18fc1d441d9
Ranjith K <thisisranjithk@gmail.com>
2025-01-06 18:08:46 +0530

    Update Seeder for Reviews

--- seeder.js
+++ seeder.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const dotenv = require("dotenv");
 const BootCamp = require("./models/BootCamp");
 const Course = require("./models/Course");
+const Review = require("./models/Review");
 
 // Load Env Variables
 dotenv.config({ path: "./config/config.env" });
@@ -16,6 +17,7 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`)
 );
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`));
 
 // Import into DB
 const importData = async () => {
@@ -24,6 +26,8 @@ const importData = async () => {
     console.log("Bootcamp data imported successfully...".yellow.inverse);
     await Course.create(courses);
     console.log("Courses data imported successfully...".yellow.inverse);
+    await Review.create(reviews);
+    console.log("Reviews data imported successfully...".yellow.inverse);
     process.exit();
   } catch (error) {
     console.log(error.red);
@@ -37,6 +41,8 @@ const deleteData = async () => {
     console.log("Bootcamp data deleted...".red.inverse);
     await Course.deleteMany();
     console.log("Course data deleted...".red.inverse);
+    await Review.deleteMany();
+    console.log("Review data deleted...".red.inverse);
     process.exit();
   } catch (error) {
     console.log(error.red);
